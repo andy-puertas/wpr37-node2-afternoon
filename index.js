0f508b5722247fc056990e95304d4e37ecf7226f
Andy Puertas <andy.puertas08@gmail.com>
2018-05-24 14:47:34 -0600

    crud sql files created

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
+massive( process.env.CONNECTION_STRING).then( dbInstance => 
+    app.set('db', dbInstance)
+)
+
 const port = process.env.PORT || 4040;
 app.listen( port, () => { 
     console.log(`Server listening on port ${port}.`);
 });
+
+// CREATE TABLE products (
+//     product_id SERIAL PRIMARY KEY NOT NULL,
+//     name VARCHAR(40) NOT NULL,
+//     description varchar(80) NOT NULL,
+//     price integer NOT NULL,
+//     image_url text NOT NULL
+// );
